2938f16610cdc41394583d424474eb3081dc80c0
Nemedik <nemedi.kristof@gmail.com>
2025-05-16 00:36:41 +0200

    Gyakorlatilag működő Blackjack, de azért még szépíteni kell

--- H6ZDVY/H6ZDVY/Blackjack.cs
+++ H6ZDVY/H6ZDVY/Blackjack.cs
@@ -34,23 +34,27 @@ public class Blackjack
         }
     }
 
-    public bool AceChecker(List<Card> deck)
+    public int AceChecker(List<Card> deck)
     {
         for (int i = 0; i < deck.Count; i++)
         {
             if (deck[i].Value == 11)
             {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
-    public bool Round()
+    public int Round() // -1 if lose, 0 if tie, 1 if win
     {
         for (int i = 0; i < deck.Length; i++)
         {
             deck[i].Drawn = false;
+            if(deck[i].Value == 1)
+            {
+                deck[i].Value = 11;
+            }
         }
         int dealerPoint = 0;
         int playerPoint = 0;
@@ -63,12 +67,14 @@ public class Blackjack
             playerCards.Add(CardDraw());
             playerPoint += playerCards[i].Value;
         }
-        if(dealerPoint > 21 && AceChecker(dealerCards))
+        if(dealerPoint > 21 && AceChecker(dealerCards) != -1)
         {
+            dealerCards[AceChecker(dealerCards)].Value = 1;
             dealerPoint -= 10;
         }
-        if (playerPoint > 21 && AceChecker(playerCards))
+        if (playerPoint > 21 && AceChecker(playerCards) != -1)
         {
+            playerCards[AceChecker(playerCards)].Value = 1;
             playerPoint -= 10;
         }
         while (true)
@@ -82,6 +88,7 @@ public class Blackjack
                     Console.Write(" and ");
                 }
             }
+            Console.WriteLine("");
             Console.WriteLine("Your cards: ");
             for (int i = 0; i < playerCards.Count; i++)
             {
@@ -93,25 +100,101 @@ public class Blackjack
             }
             Console.WriteLine("");
             Console.WriteLine("Write H for draw another card or write S to stop.");
-            if(Console.ReadLine() == "S")
+            string line = Console.ReadLine();
+            if(line == "S" || line == "s")
             {
                 break;
             }
-            else if(Console.ReadLine() == "H")
+            else if(line == "H" || line == "h")
             {
+                Card card = CardDraw();
+                playerCards.Add(card);
+                playerPoint += card.Value;
+                if(playerPoint > 21)
+                {
+                    if(AceChecker(playerCards) != -1)
+                    {
+                        playerCards[AceChecker(playerCards)].Value = 1;
+                        playerPoint -= 10;
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("You lost!");
+                        Console.WriteLine("");
+                        return -1;
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("Error!");
+                Console.WriteLine("Wrong character!");
+            }
+        }
+        while(true)
+        {
+            if(dealerPoint > 21)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("You won!");
+                Console.WriteLine("");
+                return 1;
+            }
+            if (dealerPoint >= 17)
+            {
+                if (playerPoint > dealerPoint)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You won!");
+                    Console.WriteLine("");
+                    return 1;
+                }
+                else if (playerPoint < dealerPoint)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("You lost!");
+                    Console.WriteLine("");
+                    return -1;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("The game was a tie!");
+                    Console.WriteLine("");
+                    return 0;
+                }
+            }
+            Card card = CardDraw();
+            dealerCards.Add(card);
+            dealerPoint += card.Value;
+            if(dealerPoint > 21 && AceChecker(dealerCards) != -1)
+            {
+                dealerCards[AceChecker(dealerCards)].Value = 1;
+                dealerPoint -= 10;
             }
         }
     }
 
     public void Game(int money)
     {
-        Console.WriteLine("");
-        Console.WriteLine("You can quit anytime by writing Q!");
-        Console.WriteLine("Current money: " + money);
-        Console.WriteLine("");
+        while(true)
+        {
+            Console.WriteLine("Current money: " + money);
+            Console.WriteLine("Press enter to play a round, or write Q to quit!");
+            string line = Console.ReadLine();
+            if (line == "Q" || line == "q")
+            {
+                return;
+            }
+            int match = Round();
+            if(match == 1)
+            {
+                money += 500;
+            }
+            else if(match == -1)
+            {
+                money -= 500;
+            }
+        }
     }
 }
